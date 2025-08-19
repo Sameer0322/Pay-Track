2589e4f7538d06cd3bd44525c3c9fdfb5d0cb1fc
Sameer0322 <116619265+Sameer0322@users.noreply.github.com>
2025-08-19 23:51:03 +0530

    In Progress

--- Backend/server.js
+++ Backend/server.js
@@ -18,7 +18,7 @@ app.use(cors());
 // Routes
 app.get('/', (req, res) => res.send('API is running...'));
 
-// Add route files later here
+// Route files 
 app.use('/api/auth', authRoutes);
 app.use('/api/departments', departmentRoutes);
 app.use('/api/employee', employeeRoutes);
